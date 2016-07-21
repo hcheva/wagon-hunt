11d62a8e63960d52e5fad03c438dc9849800d2d8
Henri Chevallier <henri.chevallier@gmail.com>
2016-07-21 16:42:55 +0200

    add resources routing for product actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,29 @@
 class ProductsController < ApplicationController
 
   def index
-
     @products = Product.all
-
   end
 
 def show
   index = params[:id]
   @product = Product.find(params[:id])
 end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,23 @@ Rails.application.routes.draw do
 
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
+
+
+  #read actions routes
   get "/products/:id" => "products#show"
   get "/products" => "products#index"
+
+  #create actions routes
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+
+  #update actions routes
+  get "/products/:id/edition" => "products#edit"
+  patch "/products/:id" => "products#update"
+
+  #delete actions routes
+  delete "/products/:id" => "products#destroy"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
